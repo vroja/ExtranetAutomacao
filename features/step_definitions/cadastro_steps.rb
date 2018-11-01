7eaaddb2afe23630ba825057333f514b2616bd08
victor roja tavoni <victortavoni@uol.com.br>
2018-11-01 16:42:02 -0300

    arrumado

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -17,9 +17,9 @@ Dado("que esteja logado em meu usuario") do |table2|
   Quando("preencho as informações do novo usuario com") do |table|
    @cadastro = CadastroUser.new
    @tbcadastro = table.rows_hash
-   @cadastro.cadastro(@tbCasastro[:login], @tbcadastro[:tipo], @tbcadastro[:nome], 
-   @tbcadastro[:email], @tbcadastro[:data], @tbcadastro[:area], @tbcadastro[:area2], @tbcadastro[:senha],CPF.generate(false))
+  # @cadastro.cadastro(@tbCasastro[:login], @tbcadastro[:tipo], @tbcadastro[:nome], @tbcadastro[:email], @tbcadastro[:data], @tbcadastro[:area], @tbcadastro[:area2], @tbcadastro[:senha],CPF.generate(false))
 
+   @cadastro.cadastro("CRC_II","Perfil Solicitação","CRC_II","crc_2@redetrel.com.br","06/04/1995","Funcionario","CRC","Dqm52vnc!","152")
 
 
 
