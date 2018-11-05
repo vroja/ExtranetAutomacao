15cf6b7637274149bdf0ecd93101b839e888cd59
victor roja tavoni <victortavoni@uol.com.br>
2018-11-05 08:12:41 -0200

    trocado a URL da aplicação

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,6 +5,7 @@ require 'selenium-webdriver'
 
 Capybara.configure do |c|
     c.default_driver = :selenium_chrome
-    c.app_host = 'https://portaldoponto.pagfacil.com.br'
+    #c.app_host = 'https://portaldoponto.pagfacil.com.br'
+    c.app_host = 'http://200.189.170.210/'
     c.default_max_wait_time = 10
 end
